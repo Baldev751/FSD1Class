9433788325f1fc233858f37e4cc100e9d9307a26
Baldev751 <baldev.22b1531001@abes.ac.in>
2024-10-17 20:39:09 +0530

    js work done

---
+++ jswork/index.js
@@ -0,0 +1,15 @@
+// console.log("Hello Js");
+// const a =12;
+// if(a>10){
+//   let b =23;
+//   a =40
+//   console.log("a ="+a);
+// }
+// console.log("hi,b ="+b);
+function fun(a,b){
+  console.log("heee");
+  return a+b;
+
+}
+let data = fun(23,4);
+console.log(data);
