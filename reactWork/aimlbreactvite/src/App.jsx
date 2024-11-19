89026c823901277d5670dfa04f677f5b0f8f8e24
Baldev751 <baldev.22b1531001@abes.ac.in>
2024-11-19 10:41:22 +0530

    react-work

--- reactWork/aimlbreactvite/src/App.jsx
+++ reactWork/aimlbreactvite/src/App.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
+import Student from './Student'
+import image from './assets/Thalapathy-Vijay.jpg'
 
 function App() {
-
-  let a=20;
-  let mystyle={
-    backgroundColor:'cyan',
-    color:'red',
-    border:'10px solid black',
-    height :'100px'
-    :'20px'
+  const studentdata={
+    college:"ABES Engineering College",
+    pic:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+    name:"Baldev",
+    roll:"27223",
+    branch:"CSE AIML",
+    section:"B"
   }
+
+  // let a=20;
+  // let mystyle={
+  //   backgroundColor:'cyan',
+  //   color:'red',
+  //   border:'10px solid black',
+  //   height :'100px',
+    
+  // }
   return (
-    <div style={{color:'red'}}>
-      <h2>App</h2>
-      <div style={mystyle}>{a}</div>
-      </div>
+    <div style={{marginLeft:"300px"}} >
+      <Student data={studentdata} />
+      {/* <Student
+       pic = {<img src = {image} height = {100} width = { 100}/>}
+       college="ABES Engineering College" name = "Baldev" branch = " CSE AIML" section="B" roll="58" />
+
+       <Student
+       pic = {<img src = {image} height = {100} width = { 100}/>}
+        name = "Bhagat" branch = " CSE AIML" section="B" roll="8228" />
+        <Student
+       pic = {<img src = {image} height = {100} width = { 100}/>}
+       college="ABES Engineering College" name = "Bhardwaj" branch = " CSE AIML" section="B" roll="89324" /> */}
+    </div>
   )
 }
 
---
+++ reactWork/aimlbreactvite/src/Student.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./studentstyle.css"
+
+function Student({data}) {
+  return (
+    <div className='icard'>
+      {/* {
+        JSON.stringify(data)
+      } */}
+      <table>
+        <tbody>
+          <tr><td colSpan={2}>{data.college}</td></tr>
+          <tr><td colSpan={2}><img src ={data.pic} height={100} alt='student pic'/></td></tr>
+          <tr><td> Roll:</td><td>{data.roll}</td></tr>
+          <tr><td>Name:</td><td>{data.name}</td></tr>
+          <tr><td>Branch:</td><td>{data.branch}</td></tr>
+          <tr><td>Section:</td><td>{data.section}</td></tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+// Student.defaultProps={
+//   college:"AKG Engineering College"
+// }
+export default Student
